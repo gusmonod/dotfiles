ef0c195fa6d666b40d01968ccd13253792abe761
Gus Monod <gustave.monod@insa-lyon.fr>
2016-10-06 16:15:59 +0200

    Use hard links (ln) instead of cp

--- restorehome.py
+++ restorehome.py
@@ -2,19 +2,21 @@
 
 import os
 import re
-import shutil
 import sys
 
 if sys.version_info.major == 3:
   raw_input = input
 
 for filename in os.listdir('.'):
-  if re.match(r'^home\.[a-z-]+$', filename):
-    destination = '~/' + filename[4:]
-    answer = raw_input('Overwrite {} (y/n) [n]: '.format(destination))
-    if answer == 'y':
-      print('cp ' + filename + ' ' + destination)
-      shutil.copyfile(filename, destination)
-    else:
-      print('ok, ignoring')
+  if re.match(r'^home\.[a-z-]+$', filename) and os.path.isfile(filename):
+    destination = os.path.expanduser('~/' + filename[4:])
+    try:
+      os.link(filename, destination)
+    except:
+      answer = raw_input('Overwrite {} (y/n) [n]: '.format(destination))
+      if answer == 'y':
+        os.remove(destination)
+        os.link(filename, destination)
+      else:
+        print('ok, ignoring')
 
