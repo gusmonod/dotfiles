0d7c9de491e03ffccd183e1c2977f4af0e67e174
Gus Monod <gustave.monod@insa-lyon.fr>
2016-10-06 17:08:25 +0200

    Better

--- restorehome.py
+++ restorehome.py
@@ -8,27 +8,21 @@ import sys
 if sys.version_info.major == 3:
   raw_input = input
 
-def handle_file(filename):
+def link_file(filename):
   destination = os.path.expanduser(os.path.join('~', filename))
   try:
-    # os.link(filename, destination)
-    raise ''
+    os.link(filename, destination)
   except:
     answer = raw_input('Overwrite {} (y/n) [n]: '.format(destination))
     if answer == 'y':
-      pass
-      print('rm {}'.format(destination))
-      print('ln {} {}'.format(filename, destination))
-      # os.remove(destination)
-      # os.link(filename, destination)
-    else:
-      print('ok, ignoring')
+      os.remove(destination)
+      os.link(filename, destination)
 
 for current_dir, _, filenames in os.walk('.'):
   if not current_dir.startswith('./.git'):
     for filename in filenames:
       if os.path.realpath(filename) != os.path.realpath(__file__):
         filename = os.path.join(current_dir, filename)[2:]
-        # handle_file(filename)
         print(filename)
+        link_file(filename)
 
