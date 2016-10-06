86ce30859965075a612fcd07dcb83057af46d2b6
Gus Monod <gustave.monod@insa-lyon.fr>
2016-10-06 17:02:13 +0200

    Changed whitelist to black list

--- restorehome.py
+++ restorehome.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import glob
 import os
 import re
 import sys
@@ -7,16 +8,27 @@ import sys
 if sys.version_info.major == 3:
   raw_input = input
 
-for filename in os.listdir('.'):
-  if re.match(r'^home\.[a-z-]+$', filename) and os.path.isfile(filename):
-    destination = os.path.expanduser('~/' + filename[4:])
-    try:
-      os.link(filename, destination)
-    except:
-      answer = raw_input('Overwrite {} (y/n) [n]: '.format(destination))
-      if answer == 'y':
-        os.remove(destination)
-        os.link(filename, destination)
-      else:
-        print('ok, ignoring')
+def handle_file(filename):
+  destination = os.path.expanduser(os.path.join('~', filename))
+  try:
+    # os.link(filename, destination)
+    raise ''
+  except:
+    answer = raw_input('Overwrite {} (y/n) [n]: '.format(destination))
+    if answer == 'y':
+      pass
+      print('rm {}'.format(destination))
+      print('ln {} {}'.format(filename, destination))
+      # os.remove(destination)
+      # os.link(filename, destination)
+    else:
+      print('ok, ignoring')
+
+for current_dir, _, filenames in os.walk('.'):
+  if not current_dir.startswith('./.git'):
+    for filename in filenames:
+      if os.path.realpath(filename) != os.path.realpath(__file__):
+        filename = os.path.join(current_dir, filename)[2:]
+        # handle_file(filename)
+        print(filename)
 
